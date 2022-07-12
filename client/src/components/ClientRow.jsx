9c4b2481a99e221b9ec88dbf9edcbd75e91a836f
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2022-07-12 18:23:26 +0100

    Add Project, Update project, Delete project and delete associated projects upon client delete

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -2,20 +2,23 @@ import { FaTrash } from 'react-icons/fa';
 import { useMutation } from '@apollo/client';
 import { DELETE_CLIENT } from '../mutations/clientMutations';
 import { GET_CLIENTS } from '../queries/clientQueries';
+import { GET_PROJECTS } from '../queries/projectQueries';
 
 export default function ClientRow({ client }) {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
-    // refetchQueries: [{ query: GET_CLIENTS }]
-    update(cache, { data: { deleteClient } }) {
-      const { clients } = cache.readQuery({ query: GET_CLIENTS });
-      cache.writeQuery({
-        query: GET_CLIENTS,
-        data: {
-          clients: clients.filter(client => client.id !== deleteClient.id)
-        },
-      });
-    }
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }]
+
+    // This is still valid but commented out because we want to use refetchQueries to reflect the deleted update of the deleted associated projects
+    // update(cache, { data: { deleteClient } }) {
+    //   const { clients } = cache.readQuery({ query: GET_CLIENTS });
+    //   cache.writeQuery({
+    //     query: GET_CLIENTS,
+    //     data: {
+    //       clients: clients.filter(client => client.id !== deleteClient.id)
+    //     },
+    //   });
+    // }
   });
 
   return (
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import AddClientModal from "../components/AddClientModal";
+import AddProjectModal from "../components/AddProjectModal";
 import Clients from "../components/Clients";
 import Projects from "../components/Projects";
 
@@ -6,7 +7,8 @@ export default function Home() {
   return (
     <>
       <div className="d-flex gap-3 mb-4">
-        <AddClientModal />  
+        <AddClientModal />
+        <AddProjectModal />
       </div>
       <Projects />
       <hr />
